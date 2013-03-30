40b2b884feac1a4e06435ae24c4f010028859255
Ruediger Gad <r.c.g@gmx.de>
2013-03-30 10:16:24 +0100

    Update documentation etc.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject test2junit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plug-in for emitting test output in JUnit XML format"
+  :url "https://github.com/ruedigergad/test2junit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.5.1"]])
--- src/leiningen/test2junit.clj
+++ src/leiningen/test2junit.clj
@@ -1,4 +1,5 @@
 (ns leiningen.test2junit
+  "Output test results to JUnit XML format."
   (:require [leiningen.test]
             [robert.hooke]))
 
@@ -7,7 +8,11 @@
   (apply f args))
 
 (defn test2junit
-  "Print test results to junit format."
+  "Output test results to JUnit XML format.
+  
+   This plug-in writes test results to files in JUnit XML format.
+   These files can be used, e.g., with junitreport for creating reports in HTML format.
+   Please see the webpage for more information: https://github.com/ruedigergad/test2junit"
   [project & keys]
   (robert.hooke/add-hook #'leiningen.test/form-for-testing-namespaces
                          add-test-var-println)

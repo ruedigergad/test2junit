fb20f134b4568fadeba930db0560f0d4d2cd616f
Ruediger Gad <r.c.g@gmx.de>
2013-03-30 10:23:17 +0100

    Allow configuration of output directory.

--- src/test2junit/core.clj
+++ src/test2junit/core.clj
@@ -4,14 +4,15 @@
             robert.hooke
             test2junit.junit))
 
-(defn apply-junit-output-hook []
+(defn apply-junit-output-hook [output-dir]
+  (println "Writing output to:" output-dir "\n")
   (robert.hooke/add-hook 
     #'clojure.test/test-ns
     (fn [f# & args#]
-      (clj-assorted-utils.util/mkdir "test2junit")
-      (clj-assorted-utils.util/mkdir "test2junit/html")
-      (clj-assorted-utils.util/mkdir "test2junit/tmp")
-      (clj-assorted-utils.util/mkdir "test2junit/xml")
+      (clj-assorted-utils.util/mkdir output-dir)
+      (clj-assorted-utils.util/mkdir (str output-dir "/html"))
+      (clj-assorted-utils.util/mkdir (str output-dir "/tmp"))
+      (clj-assorted-utils.util/mkdir (str output-dir "/xml"))
       (let [ns# (first args#)]
         (println "Testing:" ns#)
         (with-open [wrtr# (clojure.java.io/writer (str "test2junit/xml/" ns# ".xml"))]
--- src/test2junit/plugin.clj
+++ src/test2junit/plugin.clj
@@ -1,9 +1,12 @@
 (ns test2junit.plugin)
 
 (defn middleware [project]
-  (let [prjct (update-in project [:injections] concat
+  (let [output-dir (if (nil? (:test2junit-output-dir project))
+                     "test2junit"
+                     (:test2junit-output-dir project))
+        prjct (update-in project [:injections] concat
                        `[(require 'test2junit.core)
-                         (test2junit.core/apply-junit-output-hook)])]
+                         (test2junit.core/apply-junit-output-hook ~output-dir)])]
     (update-in prjct [:dependencies] concat
                [['robert/hooke "1.3.0"]
                 ['clj-assorted-utils "1.2.0"]

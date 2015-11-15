adb157321efa5fd21e8a9392bdb55642931e77d7
Ruediger Gad <r.c.g@gmx.de>
2015-11-15 17:52:46 +0100

    Update to latest clj-assorted-utils dependency.

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
 ;;;   http://opensource.org/licenses/eclipse-1.0.php
 ;;;
 
-(defproject test2junit "1.1.2"
+(defproject test2junit "1.1.3"
   :description "Leiningen plug-in for emitting test output in JUnit XML format"
   :url "https://github.com/ruedigergad/test2junit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-assorted-utils "1.9.1"]])
+                 [clj-assorted-utils "1.11.0"]])

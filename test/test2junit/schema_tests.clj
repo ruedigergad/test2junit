1fab0d4ce27be7b12b5fac653757827d8cc549eb
Ruediger Gad <r.c.g@gmx.de>
2017-05-25 13:07:24 +0200

    Clean up schema related tests.

--- test/test2junit/schema_tests.clj
+++ test/test2junit/schema_tests.clj
@@ -23,33 +23,6 @@
   (s/validate s/Num "1864"))
 
 ;;;
-;;; The example schema data and validation snippets were taken from the README at:
-;;; https://github.com/plumatic/schema#meet-schema
-;;;
-(def Data
-  "A schema for a nested data type"
-  {:a {:b s/Str
-       :c s/Int}
-   :d [{:e s/Keyword
-        :f [s/Num]}]})
-
-(deftest schema-validate-success
-  (s/validate
-    Data
-    {:a {:b "abc"
-         :c 123}
-     :d [{:e :bc
-          :f [12.2 13 100]}
-         {:e :bc
-          :f [-1]}]}))
-
-(deftest schema-validate-fail
-  (s/validate
-    Data
-    {:a {:b 123
-         :c "ABC"}}))
-
-;;;
 ;;; The example schema data and validation snippets are based on examples from the README at:
 ;;; https://github.com/plumatic/schema#beyond-type-hints
 ;;;
@@ -58,26 +31,9 @@
   [date :- Long
    names :- [s/Str]])
 
-(s/defn stamped-names-correct :- StampedNames
-  [names :- [s/Str]]
-  (StampedNames. (System/currentTimeMillis) names))
-
 (s/defn stamped-names-failing :- StampedNames
   [names :- [s/Str]]
   (StampedNames. (str (System/currentTimeMillis)) names))
-
-(deftest schema-explain-record
-  (s/explain StampedNames))
-
-(deftest schema-explain-correct-fn
-  (s/explain (s/fn-schema stamped-names-correct)))
-
-(deftest schema-explain-failing-fn
-  (s/explain (s/fn-schema stamped-names-failing)))
-
-(deftest schema-fn-validation-correct-fn
-  (s/with-fn-validation
-    (stamped-names-correct ["bob"])))
 
 (deftest schema-fn-validation-failing-fn
   (s/with-fn-validation

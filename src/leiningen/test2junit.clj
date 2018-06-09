5ac79b6605bd3e6a8d4dcb9acbc402e20159652d
Ruediger Gad <r.c.g@gmx.de>
2018-06-09 23:13:51 +0200

    Avoid NullPointerException when getting test2junit-version.

--- src/leiningen/test2junit.clj
+++ src/leiningen/test2junit.clj
@@ -44,7 +44,10 @@
                          add-test-var-println)
   (let [output-dir (test2junit.core/get-output-dir project)
         silent (:test2junit-silent project)
-        test2junit-version ((first (filter #(= (first %) 'test2junit/test2junit) (:plugins project))) 1)
+        leiningen-test2junit-info (first (filter #(= (first %) 'test2junit/test2junit) (:plugins project)))
+        test2junit-version (if (not (nil? leiningen-test2junit-info))
+                             (leiningen-test2junit-info 1)
+                             "n/a")
         _ (println "Using test2junit version:" test2junit-version)
         test2junit-profile [{:injections `[(require 'test2junit.core)
                                            (test2junit.core/apply-junit-output-hook ~output-dir ~silent)]

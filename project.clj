ffcbb25de9cc329471b901742002cbe68ee5e0ff
Ruediger Gad <r.c.g@gmx.de>
2022-10-02 20:41:04 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -8,16 +8,16 @@
 
 ; Remember to update the version info in src/leiningen/test2junit.clj as well.
 ; (Or find a way to automatically do this on compilation/deployment.)
-(defproject test2junit "1.4.2"
+(defproject test2junit "1.4.3"
   :description "Leiningen plug-in for emitting test output in JUnit XML format"
   :url "https://github.com/ruedigergad/test2junit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-assorted-utils "1.18.2"]]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.2.3" :exclusions  [org.clojure/clojure]]]}
-              :test {:dependencies [[prismatic/schema "1.1.6"]]}}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-assorted-utils "1.19.0"]]
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "1.3.0" :exclusions  [org.clojure/clojure]]]}
+              :test {:dependencies [[prismatic/schema "1.4.1"]]}}
   :test2junit-run-ant true
   :test2junit-output-dir "ghpages/test-results"
 )
